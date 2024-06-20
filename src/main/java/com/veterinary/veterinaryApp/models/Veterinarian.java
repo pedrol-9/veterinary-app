875a2faeaa4bd4556e259fd7255058054c716f7b
Leonelborjas <leonelborjas@outlook.es>
2024-06-20 11:17:23 -0300

    clases y dto pet y veterinario

---
+++ src/main/java/com/veterinary/veterinaryApp/models/Veterinarian.java
@@ -0,0 +1,58 @@
+package com.veterinary.veterinaryApp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Veterinarian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String specialty;
+    private Client client;
+
+    public Veterinarian(Long id, String name, String specialty, Client client) {
+        this.id = id;
+        this.name = name;
+        this.specialty = specialty;
+        this.client = client;
+    }
+
+    public Veterinarian() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
